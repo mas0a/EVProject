d42def6501a85040e7ad07481e04a9d611bbbb5d
Jayadeep Vutukuri <jayadeepv1@gmail.com>
2026-01-08 16:55:05 -0500

    fuckass code isnt working

--- mainCode/mainCode.ino
+++ mainCode/mainCode.ino
@@ -3,6 +3,7 @@ SCIOLY ELECTRIC VEHICLE CONTROLLER - ADAPTED
 Based on Unphayzed Electric Champion Kit
 Adapted for L298N motor driver and JGB37-520 encoder
 Auto-start, no LCD or buttons
+Simplified for straight-line movement
 */
 
 #include <Arduino.h>
@@ -11,7 +12,7 @@ Auto-start, no LCD or buttons
 // ========================
 // COMPETITION SETTINGS
 // ========================
-double TargetDistance = 2.000;   // meters
+double TargetDistance = 6.000;   // meters
 double TargetTime = 20.00;        // seconds
 
 // ========================
@@ -30,11 +31,9 @@ double TargetTime = 20.00;        // seconds
 // ========================
 // PHYSICAL PARAMETERS
 // ========================
-const double wheelDiameter = 7.3025;     // cm
-const double motorPPR = 44;              // JGB37-520 motor shaft
-const double gearRatio = 16.0 / 20.0;    // motor to wheel
-const double pulsesPerRev = motorPPR / gearRatio;
-const double wheelBaseCM = 61.75375;     // 2 ft + 5/16 in
+double wheelDiameter = 7.3025;     // cm
+double motorPPR = 70;              // JGB37-520 motor shaft
+double gearRatio = 16.0 / 20.0;    // motor to wheel
 
 // ========================
 // ENCODER VARIABLES
@@ -54,7 +53,6 @@ bool reachedTargetDistance = false;
 // DISTANCE VARIABLES
 // ========================
 double TargetDistanceCM;
-double ArcLength;
 double slowDownDistance;
 
 double targetEncoderValue;
@@ -81,12 +79,10 @@ double runTime;
 void setup() {
   Serial.begin(115200);
 
-  // motor pins
   pinMode(ENA, OUTPUT);
   pinMode(IN1, OUTPUT);
   pinMode(IN2, OUTPUT);
 
-  // encoder pins
   pinMode(ENCA, INPUT_PULLUP);
   pinMode(ENCB, INPUT_PULLUP);
 
@@ -94,39 +90,47 @@ void setup() {
   attachInterrupt(digitalPinToInterrupt(ENCB), ai1, RISING);
 
   TargetDistanceCM = TargetDistance * 100.0;
-  ArcLength = TargetDistanceCM;  // Set to straight-line distance
-  slowDownDistance = TargetDistanceCM * 0.8;  // Slow down at 80% of target distance
+  slowDownDistance = TargetDistanceCM * 0.8;
 
   targetEncoderValue = getEncoderValue(TargetDistanceCM, wheelDiameter);
   slowDownEncoderValue = getEncoderValue(slowDownDistance, wheelDiameter);
   maxEncoderValue = getEncoderValue(TargetDistanceCM * 2.0, wheelDiameter);
 
-  delay(3000);
+  Serial.println("=== ENCODER CALCULATIONS ===");
+  Serial.print("Target Distance: ");
+  Serial.print(TargetDistanceCM);
+  Serial.println(" cm");
+  Serial.print("Target Encoder Value: ");
+  Serial.println(targetEncoderValue);
+  Serial.print("Slow Down Encoder Value: ");
+  Serial.println(slowDownEncoderValue);
+  Serial.print("Max Encoder Value: ");
+  Serial.println(maxEncoderValue);
+  Serial.println();
 
+  delay(3000);
   counter = 0;
   startTime = millis();
   SBpressed = true;
+  Serial.println("=== READY TO START ===\n");
 }
 
 // ========================
 // MAIN LOOP
 // ========================
-
-
 void loop() {
-
-static unsigned long lastPrint = 0;
-
-if (millis() - lastPrint > 250) {
-  lastPrint = millis();
-  Serial.print("ENC = ");
-  Serial.println(counter);
-}
+  static unsigned long lastPrint = 0;
+  if (millis() - lastPrint > 250) {
+    lastPrint = millis();
+    Serial.print("ENC = ");
+    Serial.println(counter);
+  }
 
   if (SBpressed) {
-    motorForward(200);
+    motorForward(255);
     moved = true;
     SBpressed = false;
+    Serial.println("Motor started at full power");
   }
 
   if (moved) {
@@ -137,6 +141,11 @@ if (millis() - lastPrint > 250) {
       currentTime = millis();
       runTime = (currentTime - startTime) / 1000.0;
       timeDiff = TargetTime - runTime;
+
+      Serial.print("Reached slowdown point. Counter: ");
+      Serial.print(counter);
+      Serial.print(", Time remaining: ");
+      Serial.println(timeDiff);
 
       slowed = true;
       moved = false;
@@ -202,8 +211,8 @@ if (millis() - lastPrint > 250) {
 // MOTOR FUNCTIONS
 // ========================
 void motorForward(int pwm) {
-  digitalWrite(IN1, LOW);
-  digitalWrite(IN2, HIGH);
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
   analogWrite(ENA, pwm);
 }
 
@@ -230,7 +239,7 @@ void ai1() {
 // CALCULATIONS
 // ========================
 double getEncoderValue(double distCM, double wheelDiaCM) {
-  double wheelCirc = PI * wheelDiaCM;
+  double wheelCirc = 3.14159 * wheelDiaCM;
   double effectivePPR = motorPPR / gearRatio;
   return (distCM / wheelCirc) * effectivePPR;
 }
